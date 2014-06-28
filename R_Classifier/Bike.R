e1f93e9c706fcd81d811931815faa0e88858f9c8
dbajunkie <dbajunkie@gmail.com>
2014-06-28 09:24:31 -0400

    Added Predictors in R (SVM)
    Aded a set of code to change the attributes around for the data frames.

--- R_Classifier/Bike.R
+++ R_Classifier/Bike.R
@@ -1,10 +1,27 @@
+#Add Libraries
 library("e1071")
 
+#Set Globals
 setwd("C:\\Users\\Administrator\\Documents\\GitHub\\BIkeData")
+
+#Read Files
 Train = read.csv("data\\train.csv", header = TRUE)
 Test = read.csv("data\\test.csv", header = TRUE)
+attach(Train)
 
+#REarrange Structure to match SVM input
+Test = cbind(Test, Hour = as.POSIXlt(Test[,1])$hour)
+Train1 = cbind(Train, Hour = as.POSIXlt(Train[,1])$hour)
+Train2 = Train1[2:13]
+Train3 = subset(Train2, select = -casual)
+Train4 = subset(Train3, select = -registered)
+model = svm(count~., data=Train4)
 
-Model = svm(Train[2:11],Train[12],type="nu-regression")
-predict(Model,Train[12])
+#Predict using SVM model, Wirte back to Train to see results
+PredictedTrain = cbind(Train, predict = as.integer(predict(model, Train1[,-1], interval="predict")))
+PredictedTest = cbind(Test, predict = as.integer(predict(model, Test[,-1], interval="predict")))
+
+print(model)
+summary(model)
+
 
